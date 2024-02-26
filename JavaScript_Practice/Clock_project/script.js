5d48c45b2d94ff8f61b703b2f045b8734d4eb986
Kartik <iamkaranmishra551@gmail.com>
2024-02-26 22:12:51 +0530

    fix bugs.

--- JavaScript_Practice/Clock_project/script.js
+++ JavaScript_Practice/Clock_project/script.js
@@ -1,15 +1,15 @@
 const clock = document.querySelector(".box");
 
 setInterval(() => {
-    let date = new Date();
-    clock.innerHTML=date.toLocaleTimeString()
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
 
 // console.log(time.toLocaleTimeString());
 // console.log(time.toLocaleDateString());
 
 // setTimeout(() => {
-//     console.log(time.toLocaleString());  
+//     console.log(time.toLocaleString());
 // }, 1000);
 /* setInterval(() => {
     let time = new Date(); 
